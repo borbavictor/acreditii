dd75c97e87a94c35fdce6b6ea3f4f861af84217b
siriani <allan@fatecpompeia.edu.br>
2021-01-17 17:22:51 -0300

    present toast when user is updated

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FirestoreService } from '../services/firestore.service';
-
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -14,12 +14,14 @@ export class Tab3Page {
 
   private userForm: FormGroup;
   uid = "RtnbeXYABKS28NdJwuBll8UEHLQ2"
+  avatarImage = 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png';
   user;
 
   constructor(
     private router: Router,
     private firestoreService: FirestoreService,
     private formBuilder: FormBuilder,
+    private toastController: ToastController,
   ) {
     this.userForm = this.formBuilder.group({
       birthDate: [null],
@@ -34,14 +36,27 @@ export class Tab3Page {
   ionViewWillEnter() {
     this.firestoreService.getUserInfo().subscribe(user => {
       this.user = user.data()
-      this.userForm.patchValue(this.user)
       console.log(this.user)
+      this.userForm.patchValue(this.user)
+      if (this.user && this.user.imgUrl && this.user.imgUrl.length)
+        this.avatarImage = this.user.imgUrl
     })
   }
 
   update() {
     console.log("user", this.userForm.value)
     this.firestoreService.updateUser(this.userForm.value)
+    this.presentToast()
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'As alterações foram salvas',
+      duration: 3000,
+      color: 'success',
+      position: 'top'
+    });
+    toast.present();
   }
 
   logoff() {
